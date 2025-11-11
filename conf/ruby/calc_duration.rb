d373d1c87a0c6e9a060cc4a8a4d7a876ccc44154
mbabik <marian.babik@cern.ch>
2025-11-11 11:14:20 +0100

    added cric sites, scitags api, throghput, updated duration, parsing

--- conf/ruby/calc_duration.rb
+++ conf/ruby/calc_duration.rb
@@ -2,10 +2,10 @@ require 'time'
 
 def filter(event)
   begin
-    start_time = Time.parse(event.get('[flow-lifecycle][start-time]')).to_i
-    end_time = Time.parse(event.get('[flow-lifecycle][end-time]')).to_i
+    start_time = Time.parse(event.get('[flow-lifecycle][start-time]')).to_f
+    end_time = Time.parse(event.get('[flow-lifecycle][end-time]')).to_f
     if end_time >= start_time then
-      event.set('[flow-lifecycle][duration]', end_time - start_time)
+      event.set('[flow-lifecycle][duration]', (end_time - start_time).round(3))
       return [event]
     end
   rescue => exception
